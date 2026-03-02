aa42668a5611c4f9777eda9b02f5912d3c45be3e
han0208 <mphanzel@gmail.com>
2026-03-02 16:14:07 -0500

    [UPD] Comment out maintenance_equipment_views.xml in manifest

--- maintenance_project/__manifest__.py
+++ maintenance_project/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com/OCA/maintenance",
     "depends": ["base_maintenance", "project"],
     "data": [
-        "views/maintenance_equipment_views.xml",
+        # "views/maintenance_equipment_views.xml",
         "views/maintenance_request_views.xml",
         "views/project_project_views.xml",
         "report/maintenance_request_report.xml",
